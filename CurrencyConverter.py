2e411a79d71e7bf5f1dab5f396ae69ba5cb680dd
gaston <gastongithub@gmail.com>
2021-05-25 04:21:37 -0300

    cambios realizados

--- CurrencyConverter.py
+++ CurrencyConverter.py
@@ -4,5 +4,43 @@ import sys
 
 import urllib
 
-class form(QDialog):
+from PySide6.QtWidgets import *
 
+
+class Form(QDialog):
+
+    def __init__(self, parent=None):
+        super(Form, self).__init__(parent)
+
+        date = self.get_data()
+        rates = sorted(self.rates.keys())
+
+        dateLabel = QLabel(date)
+
+        self.fromComboBox = QComboBox()
+        self.toComboBox = QComboBox()
+
+        self.fromComboBox.addItems(rates)
+        self.toComboBox.addItems(rates)
+
+        self.fromSpinBox = QDoubleSpinBox()
+        self.fromSpinBox.setRange(0.01, 1000)
+        self.fromSpinBox.setValue(1.00)
+
+        self.toLabel = QLabel("1.00")
+
+        layout = QGridLayout()
+        layout.addWidget(dateLabel, 0, 0)
+        layout.addWidget(self.fromComboBox, 1, 0)
+        layout.addWidget(self.toComboBox, 2, 0)
+        layout.addWidget(self.fromSpinBox, 1, 1)
+        layout.addWidget(self.toLabel, 2, 1)
+        self.setLayout(layout)
+
+        self.fromComboBox.currentIndexChanged.connect(self.update_ui)
+
+    def get_data(self):
+        self.rates = {}
+
+    def update_ui(self):
+        pass
