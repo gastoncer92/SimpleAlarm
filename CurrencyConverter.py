115a2e0b05559103bc511a831d66d789daf77c09
gaston <gastongithub@gmail.com>
2021-05-28 17:32:07 -0300

    se crea exchange.csv se resuelve un problema para usar .csv

--- CurrencyConverter.py
+++ CurrencyConverter.py
@@ -2,8 +2,6 @@ from PySide6.QtGui import *
 from PySide6.QtCore import *
 import sys
 
-import urllib
-
 from PySide6.QtWidgets import *
 
 
@@ -38,9 +36,52 @@ class Form(QDialog):
         self.setLayout(layout)
 
         self.fromComboBox.currentIndexChanged.connect(self.update_ui)
+        self.toComboBox.currentIndexChanged.connect(self.update_ui)
+        self.fromSpinBox.valueChanged.connect(self.update_ui)
 
     def get_data(self):
         self.rates = {}
 
+        try:
+            date = "Unknown"
+
+            # como el .csv no estaba disponible en 'http://www.bankofcanada.ca/en/markets/csv/exchange_eng.csv'
+            # se procede a buscarla en 'https://web.archive.org/web/20080513231016/http://www.bankofcanada.ca/en/
+            # markets/csv/exchange_eng.csv'
+            # y a usar el paquete standar de python para tratar importar archivos .csv
+
+            fh = open('exchange.csv')
+
+            for line in fh:
+                line = line.rstrip()
+                if not line or line.startswith(("#", "Closing")):
+                    continue
+
+                fields = line.split(",")
+                if line.startswith("Date "):
+                    date = fields[-1]
+
+                else:
+                    try:
+                        value = float(fields[-1])
+                        self.rates[fields[0]] = value
+                    except ValueError:
+                        pass
+
+            return "Exchange rates date: " + date
+        except Exception as e:
+            return "Failued to download:\n%s" % e
+
     def update_ui(self):
-        pass
+        from_ = self.fromComboBox.currentText()
+        to = self.toComboBox.currentText()
+
+        result = (self.rates[from_] / self.rates[to]) * self.fromSpinBox.value()
+
+        self.toLabel.setText("%0.2f" % result)
+
+
+app = QApplication(sys.argv)
+form = Form()
+form.show()
+app.exec_()
